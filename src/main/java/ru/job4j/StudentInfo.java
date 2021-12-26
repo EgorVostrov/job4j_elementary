01da64b694c85134acf5a17c2ed3877c72c9fc5a
EgorVostrov <vostrov.egor@live.ru>
2021-12-26 20:25:11 +0400

    2

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,8 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    @SuppressWarnings("checkstyle:MethodParamPad")
-    public static void main (String[] args) {
+        public static void main(String[] args) {
         System.out.println("Egor Vostrov");
     }
 }
