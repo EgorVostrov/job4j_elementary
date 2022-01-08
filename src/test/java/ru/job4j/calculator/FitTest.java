4241a99e1990831fb79461a73d75419ec95d16a5
EgorVostrov <vostrov.egor@live.ru>
2022-01-08 23:56:40 +0400

    2. Тесты для задачи "идеальный вес" [#159458 #247719]

--- src/test/java/ru/job4j/calculator/FitTest.java
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -10,7 +10,7 @@ public class FitTest {
         short in = 180;
         double expected = 92;
         double out = Fit.manWeight(in);
-        Assert.assertEquals(expected, out, 0.01);
+        Assert.assertEquals(expected, out, 0.001);
     }
 
     @Test
@@ -18,6 +18,6 @@ public class FitTest {
         short in = 170;
         double expected = 69;
         double out = Fit.womanWeight(in);
-        Assert.assertEquals(expected, out, 0.01);
+        Assert.assertEquals(expected, out, 0.001);
     }
 }
