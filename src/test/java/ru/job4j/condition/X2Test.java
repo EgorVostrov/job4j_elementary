931fdeeb23a9c950ebad1fb8b99f6595da24db5d
EgorVostrov <vostrov.egor@live.ru>
2022-01-09 22:40:05 +0400

    1.2. Входные данные в тесте [#297750 #247712]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -39,12 +39,12 @@ public class X2Test {
     }
 
     @Test
-    public void whenA1B1C0X1Then11() {
-        int a = 10;
+    public void whenA1B1C0X1Then2() {
+        int a = 1;
         int b = 1;
         int c = 0;
         int x = 1;
-        int expected = 11;
+        int expected = 2;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
